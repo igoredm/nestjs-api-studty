770a41f9e17e0c6552d2d1b65c0eebf88d5697ef
Igor Ehlert Del Maschio <igor.maschio@kaspper.com>
2019-10-09 14:55:02 -0300

    inicio do desenvolvimento da authenticaçao da aplicaçao

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,16 @@
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { Controller, Body, Post } from '@nestjs/common';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+  ) {}
+
+  @Post('/signup')
+  signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.authService.signUp(authCredentialsDto);
+
+  }
+}
