4011ba3cb6a64a119c7f3422314b1295746e540c
Igor Ehlert Del Maschio <igor.maschio@kaspper.com>
2019-10-09 13:53:40 -0300

    Conexao com banco, Postgres com TypeORM

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -13,28 +13,9 @@ export class TasksService {
     private taskRepository: TaskRepository,
   ) { }
 
-  // getAllTasks(): Task[] {
-  //   return this.tasks;
-  // }
-
-  // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-  //   const { status, search } = filterDto;
-
-  //   let tasks = this.getAllTasks();
-
-  //   if (status) {
-  //     tasks = tasks.filter(task => task.status === status);
-  //   }
-
-  //   if (search) {
-  //     tasks = tasks.filter(task =>
-  //       task.title.includes(search) ||
-  //       task.description.includes(search),
-  //     );
-  //   }
-
-  //   return tasks;
-  // }
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    return await this.taskRepository.getTasks(filterDto);
+  }
 
   async getTaskById(id: number): Promise<Task> {
     const found = await this.taskRepository.findOne(id);
@@ -50,14 +31,27 @@ export class TasksService {
     return this.taskRepository.createTask(createTaskDto);
   }
 
-  //   this.tasks.push(task);
-  //   return task;
+    async deleteTask(id: number): Promise<void> {
+      const result = await this.taskRepository.delete(id);
+
+      if (result.affected === 0) {
+        throw new NotFoundException(`Task with id "${id}" not found`);
+      }
+  }
+
+  // Meu jeito q eu tinha feito
+  // async deleteTask(id: number): Promise<void> {
+  //   await this.getTaskById(id);
+
+  //   await this.taskRepository.delete(id);
   // }
 
-  // deleteTask(id: string): void {
-  //   const found = this.getTaskById(id);
-  //   this.tasks = this.tasks.filter(task => task.id !== found.id);
-  // }
+  async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
+    const task = await this.getTaskById(id);
+    task.status = status;
+    await task.save();
+    return task;
+  }
 
   // updateTaskStatus(id: string, status: TaskStatus): Task {
   //   const task = this.getTaskById(id);
